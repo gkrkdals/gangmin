e30eaaf8722309e3fcdc66ebea8a83fb839dab8d
gkrkdals <77611359+gkrkdals@users.noreply.github.com>
2021-02-28 23:00:17 +0900

    Update axios.js

--- src/components/utils/axios.js
+++ src/components/utils/axios.js
@@ -3,7 +3,6 @@ import axios from "axios"
 axios.defaults.baseURL = 'https://gangmin.herokuapp.com'
 axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8'
 axios.defaults.headers.post['Access-Control-Allow-Origin'] = 'https://kind-mirzakhani-4b38a2.netlify.app'
-axios.defaults.headers.post['Access-Control-Allow-Credentials'] = 'true'
 axios.defaults.withCredentials = true
 
 export default axios
