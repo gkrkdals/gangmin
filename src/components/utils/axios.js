141afab5a76c0c19c626b0f5136533feb15e7e5e
gkrkdals <77611359+gkrkdals@users.noreply.github.com>
2021-02-28 21:46:06 +0900

    Update axios.js

--- src/components/utils/axios.js
+++ src/components/utils/axios.js
@@ -2,6 +2,6 @@ import axios from "axios"
 
 axios.defaults.baseURL = 'https://hagangmin.herokuapp.com'
 axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8'
-axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*'
+axios.defaults.withCredentials = true
 
 export default axios
